0f545741c4507dd10e85dd6df5bbd08d11e5723e
danielschwarzblacky <daniel.schwarzblacky@gmail.com>
2025-12-31 10:40:51 +0100

    Update const.py

--- custom_components/zutritt_manager/const.py
+++ custom_components/zutritt_manager/const.py
@@ -7,24 +7,20 @@ PANEL_URL = "zutritt"
 PANEL_TITLE = "Zutritt"
 PANEL_ICON = "mdi:shield-key"
 
-EVENT_ZUTRITT = "esphome.zutritt_input"
-EVENT_BELL = "esphome.halle_taster"
-EVENT_ACCESS = "zutritt_manager.access"
-
 DEFAULT_STATE = {
     "sources": {
         "halle_1": {
             "door_entity": "",
-            "granted_boolean": "input_boolean.halle_keypad_granted",
-            "denied_boolean": "input_boolean.halle_keypad_denied",
-            "bell_boolean": "input_boolean.halle_klingel",
+            "granted_boolean": "",
+            "denied_boolean": "",
+            "bell_boolean": "",
             "door_pulse_s": 3
         }
     },
     "group_booleans": {
-        "chef": "input_boolean.zutritt_group_chef",
-        "lieferant": "input_boolean.zutritt_group_lieferant",
-        "mitarbeiter": "input_boolean.zutritt_group_mitarbeiter"
+        "chef": "",
+        "lieferant": "",
+        "mitarbeiter": ""
     },
     "users": []
 }
